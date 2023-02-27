289205aea18f252c9f8e9810523fae0838c8420d
ZttaR quispe oropeza <ef_jhean@hotmail.com>
2023-02-27 16:24:50 -0500

    bloques sin imprimir

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { shortDate, longDate } from '@/hooks/Fecha'
+// import datos from '@/pages/api/cuenta/lista'
 
 const Prueba = () => {
   const [datosgasto, setDatosgasto] = useState()
@@ -8,18 +10,72 @@ const Prueba = () => {
     const databla = async () => {
       const { data } = await axios.get('/api/cuenta/1/datos')
       setDatosgasto(data)
-      console.log(data)
+      // console.log(data)
     }
     databla()
   }, [])
 
+  const arr = [
+    {
+      level: 'INF',
+      model: 'A'
+    }, {
+      level: 'INF',
+      model: 'B'
+    }, {
+      level: 'INF',
+      model: 'C'
+    }, {
+      level: 'INC',
+      model: 'A'
+    }, {
+      level: 'IND',
+      model: 'A'
+    }, {
+      level: 'IND',
+      model: 'B'
+    }
+  ]
+  const contador1 = 0
+  let contador2 = 0
+
+  const guardar = () => {
+    const res = datosgasto.reduce(function (total, current) {
+      const fecha2 = shortDate(current.detDate).toString()
+      // console.log(fecha2)
+
+      if (total[fecha2]) contador2++
+      else {
+        contador2 = 0
+        total[fecha2] = { fecha: fecha2 }
+        // contador1++
+      }
+      total[fecha2][contador2] = current
+
+      // console.log(total[fecha2])
+
+      return total
+    }, {})
+    console.log(res)
+  }
+
   if (!datosgasto) return <h1>Cargando</h1>
   else {
-    console.log(datosgasto)
+    // console.log(datosgasto)
+    guardar()
   }
   return (
     <>
       <h1>Esta es la prueba</h1>
+      {
+
+      datosgasto.map((datosgasto, index) => (
+
+      <p key={index}>{ datosgasto.detDesc} </p>
+      ))
+
+      }
+
     </>
   )
 }
