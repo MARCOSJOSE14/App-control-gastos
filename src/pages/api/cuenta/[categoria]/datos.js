577ef9f5dab4ce8e66292993acc5e2f88e2e9d81
JOSE MARCOS <joseluismarcosramos@outlook.com>
2023-02-27 12:29:06 -0500

    Cambios API y Hoooks

--- src/pages/api/cuenta/[categoria]/datos.js
+++ src/pages/api/cuenta/[categoria]/datos.js
@@ -2,8 +2,8 @@ import { pool } from 'config/db'
 
 export default async function id (req, res) {
   if (req.method === 'GET') {
-    const { id } = req.query
-    const [resp] = await pool.query('select * from detCat where catID = (?) ;', [id])
+    const { categoria } = req.query
+    const [resp] = await pool.query('select * from detCat where catID = (?) ;', [categoria])
     return res.status(200).send(resp)
   } else {
     return res.status(405).end()
