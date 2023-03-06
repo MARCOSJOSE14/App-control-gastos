c3a905285f3fdbd22fbb3de6560deb966fddbc09
JOSE MARCOS <joseluismarcosramos@outlook.com>
2023-03-06 17:17:09 -0500

    context

--- src/contexts/Cuenta.js
+++ src/contexts/Cuenta.js
@@ -9,15 +9,17 @@ export function MyContextProvider ({ children }) {
   const [ctxMensaje, setCtxMensaje] = useState({ a: false, b: 0, c: () => (console.log('agrega funcion')) })
   const [ctxCuenta, setCtxCuenta] = useState()
 
+  const [contCat, setContCat] = useState({ catId: 0, catDesc: 'Nombre', cueId: 0 })
   const [nuevoI, setNuevoI] = useState(1)
 
   const ctxCamTos = (datoa, datob) => setCtxTostada({ a: datoa, b: datob })
   const ctxCamMen = (datoa, datob, datoc) => setCtxMensaje({ a: datoa, b: datob, c: datoc })
   const ctxCamCue = (dato) => setCtxCuenta(dato)
 
+  const camCat = (nuevacat) => setContCat(nuevacat)
   const camNue = (nuevo) => setNuevoI(nuevoI + 1)
 
-  return <MyContext.Provider value={{ nuevoI, camNue, ctxMensaje, ctxCamMen, ctxCamCue, ctxCuenta, ctxTostada, ctxCamTos }}>{children}</MyContext.Provider>
+  return <MyContext.Provider value={{ contCat, camCat, nuevoI, camNue, ctxMensaje, ctxCamMen, ctxCamCue, ctxCuenta, ctxTostada, ctxCamTos }}>{children}</MyContext.Provider>
 }
 
 export default MyContext
