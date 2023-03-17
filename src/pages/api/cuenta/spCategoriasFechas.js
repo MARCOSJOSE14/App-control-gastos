bcf96e2c2d5ea2e81b648bc8993c803a3cb80d35
ZttaR quispe oropeza <ef_jhean@hotmail.com>
2023-03-17 11:00:51 -0500

    sd

---
+++ src/pages/api/cuenta/spCategoriasFechas.js
@@ -0,0 +1,13 @@
+import { pool } from 'config/db'
+
+export default async function fechas (req, res) {
+  if (req.method === 'POST') {
+    const { fechaInicio, fechaFinal } = req.body
+    // console.log(req)
+    console.log(req.body)
+    const [resp] = await pool.query('CALL Sp_Categorias_Fechas(?, ?);', [fechaInicio, fechaFinal])
+    return res.status(200).send(resp)
+  } else {
+    return res.status(405).end()
+  }
+}
