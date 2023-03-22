259dfca4e3e6c7d28822f7df8dc81cc12468be13
JOSE MARCOS <joseluismarcosramos@outlook.com>
2023-03-22 13:23:19 -0500

    Titulo se actualiza

--- src/components/cuenta/Transacc/Titulo..jsx
+++ src/components/cuenta/Transacc/Titulo..jsx
@@ -1,12 +1,15 @@
 import TituloNav from '@/components/plantiila/TituloNav'
+import { contexto } from '@/contexts/Cuenta'
 import { apiSure } from '@/hooks/apiSure'
 import { pen } from '@/hooks/Fecha'
 import { hooApi } from '@/hooks/hooApi'
 import Toast from '@/hooks/Toast'
 
 const Titulo = ({ numero }) => {
+  const { ctxTostada } = contexto()
+
   const datainfo = apiSure(`cuenta/titulo/?id=${numero}`)
-  const saldo = hooApi(`cuenta/${numero}/apiSaldoActual`)
+  const saldo = hooApi(`cuenta/${numero}/apiSaldoActual`, '', 'GET', [ctxTostada])
 
   if (!datainfo || saldo === undefined) return Toast(true, 1)
   return (
